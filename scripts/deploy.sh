cecc09dc5092d03aa2b922bde39ab1fd73400eb0
sshan96 <sshan96@naver.com>
2022-06-03 09:01:20 +0900

    배포 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep java| awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -23,7 +23,7 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v plain | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
